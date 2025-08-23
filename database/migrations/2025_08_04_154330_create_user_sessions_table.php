601822054d0d30520ebb4e23d2b645e94538c81d
Ali Hamza Majeed <113177470+ali-hamza-cbt@users.noreply.github.com>
2025-08-23 14:45:36 +0500

    Db Changes

--- database/migrations/2025_08_04_154330_create_user_sessions_table.php
+++ database/migrations/2025_08_04_154330_create_user_sessions_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('user_sessions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
             $table->string('device_name')->nullable();
             $table->string('browser_name')->nullable();
             $table->string('ip_address');
